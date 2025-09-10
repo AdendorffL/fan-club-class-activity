38a2ae95a3a53e2931a32592bc0aa3e3c6d4d12b
zian <zian.bockel@gmail.com>
2025-09-10 08:32:19 +0200

    added dynamic text for theme button and added default theme

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const root = document.documentElement;
 button.addEventListener("click", () => {
   const currentTheme = root.getAttribute("data-theme");
   const newTheme = currentTheme === "light" ? "dark" : "light";
+  button.innerText = currentTheme === "light" ? "Light" : "Dark";
   root.setAttribute("data-theme", newTheme);
 });
